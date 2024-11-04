a206057cce77d0644ad47d0dc5237ce3ce0a08c1
danhdtpc05316 <danhdtpc05316@gmail.com>
2024-11-04 18:24:54 +0700

    fix lỗi

--- src/main/java/com/poly/service/VoucherBillService.java
+++ src/main/java/com/poly/service/VoucherBillService.java
@@ -23,34 +23,7 @@ public class VoucherBillService {
 		
 	}
 	
-	//sửa voucher bill
-	public VoucherEntity updateVoucherEntity(int id, VoucherEntity updateVoucherEntity) {
-		Optional<VoucherEntity> optionalVoucherEntity = voucherBillRepository.findById(id);
-		
-		if(optionalVoucherEntity.isPresent()){
-			VoucherEntity voucherEntity = optionalVoucherEntity.get();
-			voucherEntity.setGiamGia(updateVoucherEntity.getGiamGia());
-			voucherEntity.setSoLuong(updateVoucherEntity.getSoLuong());
-			voucherEntity.setNgaybatdau(updateVoucherEntity.getNgaybatdau());
-			voucherEntity.setNgayHetHan(updateVoucherEntity.getNgayHetHan());
-			voucherEntity.setDonhang(updateVoucherEntity.getDonhang());
-			return voucherBillRepository.save(voucherEntity);
-		}else {
-			throw new RuntimeException("voucherBill not found with id "+ id);
-		}
-		
-	}
-	//xoá voucherBill
 	
-	public void deleteVoucherBill(int id) {
-        voucherBillRepository.deleteById(id);
-    }
-	
-	// Lấy danh sách tất cả VoucherBills
-	
-	public List<VoucherEntity> getAllVoucherBills() {
-        return voucherBillRepository.findAll();
-    }
 		
 	
 }
