b61783c0d8a7e6ca7b7218a2e79c01e656f5459f
quangtt-pc06371 <quangttpc06371@fpt.edu.vn>
2024-11-08 22:09:17 +0700

    update shop

--- src/main/java/com/poly/controller/ShopController.java
+++ src/main/java/com/poly/controller/ShopController.java
@@ -23,15 +23,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poly.DtoEntity.ErrorResponse;
 import com.poly.DtoEntity.ShopDTO;
 import com.poly.entity.ShopEntity;
 import com.poly.repository.ShopRepository;
@@ -115,7 +113,7 @@ public class ShopController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ShopEntity> registerShop(
+    public ResponseEntity<?> registerShop(
             @RequestParam("shopName") String shopName,
             @RequestParam("shopDescription") String shopDescription,
             @RequestParam(value = "shopImage", required = false) MultipartFile shopImage,
@@ -127,7 +125,7 @@ public class ShopController {
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
-        
+
         int userId = jwtSevice.getIdFromToken(token);
         if (userId == 0) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
@@ -142,9 +140,11 @@ public class ShopController {
             ShopEntity shop = shopService.registerShop(shopDTO, shopImage);
             return ResponseEntity.ok(shop);
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(null);
+            ErrorResponse errorResponse = new ErrorResponse("Người dùng đã có cửa hàng");
+            return ResponseEntity.badRequest().body(errorResponse);
         }
     }
+
 
     // Duyệt shop
     @PutMapping("/approve/{id}")
