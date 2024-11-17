5ac5e2521452833909a63590027631f7b244f8cd
toanphan15 <toanptpc06449@fpt.edu.vn>
2024-11-18 04:57:56 +0700

    fix

---
+++ src/main/java/com/poly/controller/ShopSanPhamController.java
@@ -0,0 +1,55 @@
+package com.poly.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poly.entity.ShopEntity;
+import com.poly.service.ShopSanPhamService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/shop")
+public class ShopSanPhamController {
+	@Autowired
+    private ShopSanPhamService shopService;
+
+    @GetMapping
+    public List<ShopEntity> getAllShop() {
+        return shopService.getAllShop();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShopEntity> getShopById(@PathVariable int id) {
+        Optional<ShopEntity> optionalShop = shopService.getShopById(id);
+
+        if (optionalShop.isPresent()) {
+            return ResponseEntity.ok(optionalShop.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ShopEntity createShop(@RequestBody ShopEntity shop) {
+        return shopService.saveShop(shop);
+    }
+
+ 
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteShop(@PathVariable int id) {
+        shopService.deleteShopById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
