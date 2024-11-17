4662886d1fe4c15093ae01c553c65978e127492f
quangtt-pc06371 <quangttpc06371@fpt.edu.vn>
2024-11-17 15:35:00 +0700

    fix

--- src/main/java/com/poly/service/GioHangService.java
+++ src/main/java/com/poly/service/GioHangService.java
@@ -4,10 +4,10 @@ package com.poly.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.poly.entity.GioHang;
+import com.poly.entity.GioHangEntity;
 
 @Service
 public class GioHangService {
